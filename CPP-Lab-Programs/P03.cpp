6905533f70c55d13cc8a69206b5da999dcef5c47
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2025-12-23 11:26:54 +0530

    Update P03.cpp

--- CPP-Lab-Programs/P03.cpp
+++ CPP-Lab-Programs/P03.cpp
@@ -1,4 +1,4 @@
-// Write a C++ program to show Basic Input/Output and Arithmetic Operations.5
+// Write a C++ program to show Basic Input/Output and Arithmetic Operations.
 
 #include <iostream>
 using namespace std;
