65db4e384edf853dc09daaa61a050c100ff49ec2
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2026-01-21 16:01:02 +0530

    updated

--- CPP-Class-Programs/02 Classses n Objects/ConstDest01.cpp
+++ CPP-Class-Programs/02 Classses n Objects/ConstDest01.cpp
@@ -5,7 +5,7 @@ class Line {
    public:
       void setLength( double len );
       double getLength( void );
-      Line();   // This is the constructor declaration
+      Line(int l);   // This is the constructor declaration
       ~Line();  // This is the destructor: declaration
  
    private:
@@ -13,8 +13,9 @@ class Line {
 };
  
 // Member functions definitions including constructor
-Line::Line(void) {
-   cout << "Object is being created" << endl;
+Line::Line(int l) {
+   length = l;
+   cout << "Object "<<l<<" is being created" << endl;
 }
 Line::~Line(void) {
    cout << "Object "<<length<<" is being deleted" << endl;
@@ -27,21 +28,21 @@ double Line::getLength( void ) {
 }
 
 void function1(){
-    Line line2;
-    line2.setLength(2.0);
+    Line line2(2);
+   //  line2.setLength(2.0);
     cout << "Length of line2 : " << line2.getLength() <<endl;
 }
 
 // Main function for the program
 int main() {
-   Line line1;
-   line1.setLength(1.0); 
+   Line line1(1); // Line object created
+   // line1.setLength(1.0); 
    cout << "Length of line : " << line1.getLength() <<endl;
    
    function1();
 
-   Line line3;
-   line3.setLength(3.0); 
+   Line line3(3);
+   // line3.setLength(3.0); 
    cout << "Length of line : " << line3.getLength() <<endl;
    return 0;
 }
--- CPP-Class-Programs/02 Classses n Objects/tempCodeRunnerFile.cpp
+++ CPP-Class-Programs/02 Classses n Objects/tempCodeRunnerFile.cpp
@@ -1,1 +1,4 @@
-length
+
+   Line line3(3);
+   // line3.setLength(3.0); 
+   cout << "Leng
